e9b1ebb6a409f91622a135c8cdb675fe253c0f1f
amit-nic <projectatnic@gmail.com>
2022-08-27 16:50:02 +0530

    javascript1

---
+++ javascript/arrays.js
@@ -0,0 +1,11 @@
+const t=[1,-1,3]
+t.push(5)
+console.log(t.length)
+console.log(t[1])
+t.forEach(value=>{
+    console.log(value)
+})
+const a=[1,-1,3]
+const t2=a.concat(5)
+console.log(a)
+console.log(t2)
---
+++ javascript/variables.js
@@ -0,0 +1,8 @@
+const x=1
+let y=5
+console.log(x,y)
+y+=10
+console.log(1,15)
+y='sometext'
+console.log(x,y)
+x=4
